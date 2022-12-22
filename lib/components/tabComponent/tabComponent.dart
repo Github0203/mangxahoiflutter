7ecdaa59557066fbf866779908037f3fbd207e45
Github0203 <nhocngoc0203@gmail.com>
2022-12-22 17:15:35 +0700

    hinh anh p1

--- lib/components/tabComponent/tabComponent.dart
+++ lib/components/tabComponent/tabComponent.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:mangxahoi/components/chooseimage/chooseImage.dart';
 import 'package:scroll_navigation/scroll_navigation.dart';
 
 class tabComponent extends StatefulWidget {
@@ -11,47 +12,51 @@ class tabComponent extends StatefulWidget {
 
 class _tabComponentState extends State<tabComponent> {
   final ScrollController controller = ScrollController();
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TitleScrollNavigation(
-            barStyle: const TitleNavigationBarStyle(
-              style: TextStyle(fontWeight: FontWeight.bold),
-              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-              spaceBetween: 40,
-            ),
-            // ignore: prefer_const_literals_to_create_immutables
-            titles: [
-              "Main Page",
-              "Personal Information",
-              "Personalization",
-              "Security",
-              "Payment Methods",
-            ],
-            pages: [
-              ListView.builder(
-                itemCount: 30,
-                controller: controller,
-                padding: EdgeInsets.zero,
-                itemBuilder: (_, __) {
-                  return Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Container(
-                      height: 60,
-                      color: Colors.blue[50],
-                    ),
-                  );
-                },
-              ),
-              Container(color: Colors.red[50]),
-              Container(color: Colors.green[50]),
-              Container(color: Colors.purple[50]),
-              Container(color: Colors.yellow[50]),
-            ],
+        barStyle: const TitleNavigationBarStyle(
+          style: TextStyle(fontWeight: FontWeight.bold),
+          padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+          spaceBetween: 40,
+        ),
+        // ignore: prefer_const_literals_to_create_immutables
+        titles: [
+          "Main Page",
+          "Personal Information",
+          "Personalization",
+          "Security",
+          "Payment Methods",
+        ],
+        pages: [
+          ListView.builder(
+            itemCount: 30,
+            controller: controller,
+            padding: EdgeInsets.zero,
+            itemBuilder: (_, __) {
+              return Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Container(
+                  height: 60,
+                  color: Colors.blue[50],
+                ),
+              );
+            },
           ),
+          Container(
+            child: const chooseImage(),
+          ),
+          Container(
+            child: const chooseImage(),
+          ),
+          // Container(color: Colors.red[50]),
+          // Container(color: Colors.green[50]),
+          Container(color: Colors.purple[50]),
+          Container(color: Colors.yellow[50]),
+        ],
+      ),
     );
   }
 }
-
-
